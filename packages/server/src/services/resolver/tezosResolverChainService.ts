3f1f84be5147a922cfbd24929578c5de2998a9ea
Andrew Skubarenko <andrew.skubarenko@gmail.com>
2025-08-02 22:16:14 +0300

    Refactor deploy methods in TezosResolverChainService; consolidate logic into a single deploy function and improve parameter handling

--- packages/server/src/services/resolver/tezosResolverChainService.ts
+++ packages/server/src/services/resolver/tezosResolverChainService.ts
@@ -15,60 +15,11 @@ export class TezosResolverChainService {
   }
 
   async deploySrc(immutables: Immutables, signature: string): Promise<readonly [tx: Transaction, escrowAddress: string]> {
-    const escrowFactoryContract = await this.getEscrowFactoryContract();
-    const params = {
-      order_hash: immutables.orderHash.replace('0x', ''),
-      hashlock: immutables.hashLock.replace('0x', ''),
-      maker: immutables.maker,
-      taker: immutables.taker,
-      token: immutables.tokenId
-        ? {
-          fA2: {
-            0: immutables.token,
-            1: immutables.tokenId,
-          },
-        }
-        : immutables.token === 'tez'
-          ? {
-            tEZ: undefined,
-          }
-          : {
-            fA12: immutables.token,
-          },
-      amount: immutables.amount.toString(),
-      safety_deposit: immutables.safetyDeposit.toString(),
-      timelocks: this.prepareTimeLocks(immutables.timeLocks),
-    } as const;
-
-    const operation = escrowFactoryContract.methodsObject.deploy_src!(params);
-    const tx = await operation.send({
-      amount: Number(immutables.safetyDeposit.toString()) / 1_000_000,
-    });
-    await tx.confirmation(1);
-
-    const escrowAddress = (tx.operationResults[0] as any).metadata.internal_operation_results[0].result.originated_contracts[0];
-
-    return [
-      {
-        hash: tx.hash,
-        block: tx.includedInBlock.toString(),
-        timestamp: this.getCurrentOperationTimestamp(),
-      },
-      escrowAddress,
-    ];
+    return this.deploy('src', immutables, signature);
   }
 
   async deployDst(immutables: Immutables): Promise<readonly [tx: Transaction, escrowAddress: string]> {
-    await utils.wait(1000);
-
-    return [
-      {
-        hash: 'o1-dst',
-        block: 'b1-dst',
-        timestamp: this.getCurrentOperationTimestamp(),
-      },
-      'tz1-dst-escrow-address',
-    ];
+    return this.deploy('dst', immutables, '');
   }
 
   async withdraw(escrowAddress: string, secret: string, immutables: Immutables): Promise<Transaction> {
@@ -96,21 +47,63 @@ export class TezosResolverChainService {
     };
   }
 
+  protected async deploy(side: 'src' | 'dst', immutables: Immutables, signature?: string): Promise<readonly [tx: Transaction, escrowAddress: string]> {
+    const escrowFactoryContract = await this.getEscrowFactoryContract();
+    const params = this.getDeployParams(immutables, signature);
+
+    const operation = escrowFactoryContract.methodsObject[side === 'src' ? 'deploy_src' : 'deploy_dst']!(params);
+    const tx = await operation.send({
+      amount: Number(immutables.safetyDeposit.toString()) / 1_000_000,
+    });
+    await tx.confirmation(1);
+
+    const escrowAddress = (tx.operationResults[0] as any).metadata.internal_operation_results[0].result.originated_contracts[0];
+
+    return [
+      {
+        hash: tx.hash,
+        block: tx.includedInBlock.toString(),
+        timestamp: this.getCurrentOperationTimestamp(),
+      },
+      escrowAddress,
+    ];
+  }
+
   private getEscrowFactoryContract() {
     return this.tezosChainAccount.tezosToolkit.contract.at(this.escrowFactoryAddress);
+  }
+
+  private getDeployParams(immutables: Immutables, signature?: string) {
+    return {
+      order_hash: immutables.orderHash.replace('0x', ''),
+      hashlock: immutables.hashLock.replace('0x', ''),
+      maker: immutables.maker,
+      taker: immutables.taker,
+      token: immutables.tokenId
+        ? {
+          fA2: {
+            0: immutables.token,
+            1: immutables.tokenId,
+          },
+        }
+        : immutables.token === 'tez'
+          ? {
+            tEZ: undefined,
+          }
+          : {
+            fA12: immutables.token,
+          },
+      amount: immutables.amount.toString(),
+      safety_deposit: immutables.safetyDeposit.toString(),
+      timelocks: this.prepareTimeLocks(immutables.timeLocks),
+    } as const;
   }
 
   protected getCurrentOperationTimestamp() {
     return BigInt(Math.floor(Date.now() / 1000));
   }
 
-  private prepareTimeLockValue(value: bigint, deployedAt: bigint): string {
-    return ((deployedAt + value) * 1000n).toString();
-  }
-
   private prepareTimeLocks(timeLocks: Immutables['timeLocks']) {
-    const deployedAt = timeLocks.deployedAt ? timeLocks.deployedAt : this.getCurrentOperationTimestamp();
-
     return {
       src_withdrawal: timeLocks.srcWithdrawal,
       src_public_withdrawal: timeLocks.srcPublicWithdrawal,
